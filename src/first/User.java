ea2e4bcc5c7cb80358ba95add2f767c805669ac5
Wonyoung Choi <wnnwon@gmail.com>
2019-07-26 02:07:08 +0900

    adduser

---
+++ src/first/User.java
@@ -0,0 +1,54 @@
+package first;
+
+public class User {
+   private String user_id;
+   private String user_pw;
+   private String user_name;
+   private String user_birthdate;
+   private String gender;
+   private String phone_num;
+   private String reg_date;
+   
+public String getUser_id() {
+	return user_id;
+}
+public void setUser_id(String user_id) {
+	this.user_id = user_id;
+}
+public String getUser_pw() {
+	return user_pw;
+}
+public void setUser_pw(String user_pw) {
+	this.user_pw = user_pw;
+}
+public String getUser_name() {
+	return user_name;
+}
+public void setUser_name(String user_name) {
+	this.user_name = user_name;
+}
+public String getUser_birthdate() {
+	return user_birthdate;
+}
+public void setUser_birthdate(String user_birthdate) {
+	this.user_birthdate = user_birthdate;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getPhone_num() {
+	return phone_num;
+}
+public void setPhone_num(String phone_num) {
+	this.phone_num = phone_num;
+}
+public String getReg_date() {
+	return reg_date;
+}
+public void setReg_date(String reg_date) {
+	this.reg_date = reg_date;
+}
+}
